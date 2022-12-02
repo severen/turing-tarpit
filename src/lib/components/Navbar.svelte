fb30e6f8d4ef9aa83d71c64226802b88a9319c3f
Severen Redwood <me@severen.dev>
2022-12-02 14:27:22 +1300

    Implement a basic site layout

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,32 @@
+<!-- @license
+  SPDX-FileCopyrightText: 2022 Severen Redwood <me@severen.dev>
+  SPDX-FileCopyrightText: 2022 Keetley Rate <fridgeineyes@gmail.com>
+  SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const links = [
+    { name: "λ-calculus", url: "/lambda" },
+    { name: "Turing Machines", url: "/turing" },
+    { name: "Automata", url: "/automata" },
+  ];
+</script>
+
+<div class="mb-4 flex justify-between">
+  <h1 class="font-semibold">
+    <a href="/">The Turing Tarpit</a>
+  </h1>
+
+  <nav>
+    <ul class="flex space-x-3">
+      {#each links as { name, url }}
+        {#if $page.url.pathname !== url}
+          <li><a href={url} class="align-middle">{name}</a></li>
+        {:else}
+          <li><a href={url} class="align-middle text-red-400">{name}</a></li>
+        {/if}
+      {/each}
+    </ul>
+  </nav>
+</div>
