75237a578cb5f0bfcfc524bc9cd0d117be8b5b28
Severen Redwood <me@severen.dev>
2022-12-06 02:42:31 +1300

    Expand the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,22 @@
   SPDX-FileCopyrightText: 2022 Keetley Rate <fridgeineyes@gmail.com>
   SPDX-License-Identifier: AGPL-3.0-or-later
 -->
+<script lang="ts">
+  import { applications } from "$lib/stores/applications";
+</script>
 
-<p>There's nothing here yet, but just you wait!</p>
+<!-- TODO: Make this prettier. -->
+<p>Welcome to the Turing Tarpit!</p>
+
+<br />
+
+<p>Choose your application:</p>
+<ul class="ml-4">
+  {#each $applications as { name, description, url }}
+    <li>
+      <a class="hover:text-red-700 dark:hover:text-red-200" href={url}>
+        {name} &mdash; {description}
+      </a>
+    </li>
+  {/each}
+</ul>
