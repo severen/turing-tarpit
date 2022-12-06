4934d69eadd8012c2ea976377fc2b80f8085cba5
goose_swansworth <fridgeineyes@gmail.com>
2022-12-06 22:59:57 +1300

    Add more Turing machine tests, factor and comment turing.ts

--- src/lib/turing/turing.ts
+++ src/lib/turing/turing.ts
@@ -1,11 +1,26 @@
+// Turing Machine struct
+// delta is a transition function mapping states to maps mapping tape symbols to transision arcs
+export type TM = {
+  start_state: string;
+  accept_states: Set<string>;
+  delta: Map<string, Map<string, TM_Arc>>;
+};
 
-
-export type TM = {start_state: string, accept_states: Set<string>, delta: Map<string, Map<string, TM_Arc>>}
-type TM_Arc = {write: string; move: string; next: string}
-type TM_Update = {next_state: string, head_move: number}
-type TM_Result = {accept: string, on_tape: string}
-
-const new_arc = (write: string, move: string, next: string): TM_Arc => ({write, move, next});
+// Struct for defining transisions in a TM
+type TM_Arc = { write: string; move: string; next: string };
+// Struct for specifing a step in a TM's excution
+type TM_Update = { next_state: string; head_move: number };
+/** Struct for defining the result of TM's excution.
+ *   - accept field is 'ACCEPT' or 'REJECT' based on whether the input was accepted by the TM
+ *   - on_tape is a string of symbols that are left on the tape after the TM has halted
+ */
+type TM_Result = { accept: string; on_tape: string };
+// Return and new transition with the given fields
+const new_arc = (write: string, move: string, next: string): TM_Arc => ({
+  write,
+  move,
+  next,
+});
 
 const STATE_NAME = new RegExp("[A-Z||a-z]+[0-9]*");
 const BLANK = "_";
@@ -13,120 +28,143 @@ const TAPE_CHUNK_LEN = 10;
 const REJECT = "REJECT";
 const ACCEPT = "ACCEPT";
 
-function check_delta(delta: Map<string, Map<string, TM_Arc>>, accept_states: Set<string>) {
-  for (let state of delta.keys()) {
-    let arcs = delta.get(state)!;
-    for (let read_symbol of arcs.keys()) {
-      let arc = arcs.get(read_symbol)!;
+function check_delta(
+  delta: Map<string, Map<string, TM_Arc>>,
+  accept_states: Set<string>,
+) {
+  for (const state of delta.keys()) {
+    const arcs = delta.get(state)!;
+    for (const read_symbol of arcs.keys()) {
+      const arc = arcs.get(read_symbol)!;
       if (!delta.has(arc.next) && !accept_states.has(arc.next)) {
-        throw Error(`Transision (${state}, ${read_symbol}, ${arc.write}, ${arc.move}, ${arc.next}) ends in a non-existant state`);
+        throw Error(
+          `Transision (${state}, ${read_symbol}, ${arc.write}, ${arc.move}, ${arc.next}) ends in a non-existant state`,
+        );
       }
     }
   }
 }
 
 function read_transistion(linenum: number, line: string): Array<string> {
-  let items = line.split(" ");
-  if (items.length != 5) {
+  const items = line.split(" ");
+  if (items.length !== 5) {
     throw Error(`Line ${linenum}: Line must have five items`);
   }
-  let [state, read, write, move, next] = items;
-  if (state.search(STATE_NAME) != 0) {
+  const [state, read, write, move, next] = items;
+  if (state.search(STATE_NAME) !== 0) {
     throw Error(`Line ${linenum}: State name ${state} is not valid`);
   }
-  if (move != "L" && move != "R") {
+  if (move !== "L" && move !== "R") {
     throw Error(`Line ${linenum}: Move symbol must be "L" or "R", found ${items[3]}`);
   }
-  if (next.search(STATE_NAME) != 0) {
+  if (next.search(STATE_NAME) !== 0) {
     throw Error(`Line ${linenum}: State name ${state} is not valid`);
   }
   return [state, read, write, move, next];
 }
 
 /** Returns a new TM object from a transistion table of the form
-*     StartState AcceptState1 AcceptState2 ...
-*     State ReadSymbol WriteSymbol HeadMove NextState
-*     ...
-*   Throws errors when:
-*     - The table as a line (excluding the header) without 5 items.
-*     - The HeadMove field in a transision is not "L" or "R".
-*     - The table contains multiple transisions for a state for the same tape symbol.
-*     - The table contains a transision with Nextstate that does not appear elsewhere
-*       in the table.
-*     - A state name doesn't start with a charater
-*/
+ *     StartState AcceptState1 AcceptState2 ...
+ *     State ReadSymbol WriteSymbol HeadMove NextState
+ *     ...
+ *   Throws errors when:
+ *     - The table as a line (excluding the header) without 5 items.
+ *     - The HeadMove field in a transision is not "L" or "R".
+ *     - The table contains multiple transisions for a state for the same tape symbol.
+ *     - The table contains a transision with Nextstate that does not appear elsewhere
+ *       in the table.
+ *     - A state name doesn't start with a charater
+ */
 
 export function read_transtion_table(table: string): TM {
-  for (let name of table.split("\n")[0].split(" ")) {
-    if (name.search(STATE_NAME) != 0) {
+  for (const name of table.split("\n")[0].split(" ")) {
+    if (name.search(STATE_NAME) !== 0) {
       throw Error(`Line 1: State name ${name} is not valid`);
     }
   }
-  let header = table.split("\n")[0];
-  let start_state = header.split(" ")[0];
-  let accept_states = new Set(header.split(" ").slice(1));
-  let delta = new Map();
-  for (let [linenum, line] of table.split("\n").slice(1).entries()) {
-    line = line.trimStart();
-    let [state, read, write, move, next] = read_transistion(linenum, line);
+  const header = table.split("\n")[0];
+  const start_state = header.split(" ")[0];
+  const accept_states = new Set(header.split(" ").slice(1));
+  const delta = new Map();
+  for (const [linenum, line] of table.split("\n").slice(1).entries()) {
+    const trimline = line.trimStart();
+    const [state, read, write, move, next] = read_transistion(linenum, trimline);
     if (delta.has(state)) {
-      let reads: Map<string, TM_Arc> = delta.get(state);
+      const reads: Map<string, TM_Arc> = delta.get(state);
       if (reads.has(read)) {
-        throw Error(`Line ${linenum}: State ${state} already has a transiton for tape symbol ${read}`);
+        throw Error(
+          `Line ${linenum}: State ${state} already has a transiton for tape symbol ${read}`,
+        );
       }
-      reads.set(read, new_arc(write, move, next))
+      reads.set(read, new_arc(write, move, next));
     } else {
-      let reads = new Map();
+      const reads = new Map();
       reads.set(read, new_arc(write, move, next));
       delta.set(state, reads);
     }
   }
   check_delta(delta, accept_states);
-  return {start_state, accept_states, delta};
+  return { start_state, accept_states, delta };
 }
 
 function init_tape(input: string): Array<string> {
   let tape_length = TAPE_CHUNK_LEN;
-  while (tape_length - input.length < 2*TAPE_CHUNK_LEN) {
+  while (tape_length - input.length < 2 * TAPE_CHUNK_LEN) {
     tape_length += TAPE_CHUNK_LEN;
   }
   return Array(tape_length).fill(BLANK);
 }
 
 function extend_tape(head: number, tape: Array<string>): Array<string> {
-  if (head < 0) { //head has fallen off the left of the tape, so the head is now at the right end of the new chunk.
+  if (head < 0) {
+    //head has fallen off the left of the tape, so the head is now at the right end of the new chunk.
     return Array(TAPE_CHUNK_LEN).fill(BLANK).concat(tape);
   }
   //head has fallen off the right of the tape, just append a new chunk.
   return tape.concat(Array(TAPE_CHUNK_LEN).fill(BLANK));
-  
 }
 
 function should_trim(slot: string, i: number, tape: Array<string>): boolean {
-  if (slot != BLANK) {
+  if (slot !== BLANK) {
     return false;
   }
-  if (i > 0 && i < tape.length && tape[i - 1] != BLANK && tape[i + 1] != BLANK) {
+  if (i > 0 && i < tape.length && tape[i - 1] !== BLANK && tape[i + 1] !== BLANK) {
     return false;
   }
   return true;
 }
 
-/** Returns a (new_state, head_move) pair resulting from one step of the TM exitution on the given tape.
-*   Also updates the tape appropriately in place. If the TM reads a symbol off the tape that does not
-*   have an assioated transistion from the current state the next_state will be REJECT.
-*/
-export function tm_step(tm: TM, state: string, tape: Array<string>, head: number): TM_Update {
-    let state_lookup = tm.delta.get(state)!; // delta.get(state) is garanteed to exist as it is checked in check_delta.
-    let transistion = state_lookup.get(tape[head]);
-    if (transistion === undefined) {
-      return {next_state: REJECT, head_move: 0};
-    }
-    tape[head] = transistion!.write;
-    return {next_state: transistion!.next, head_move: transistion!.move == "R" ? 1 : -1};
+export function tape_string(tape: Array<string>, head: number): string {
+  const spaces = Array(tape.length).fill(" ");
+  const temp = [...spaces];
+  temp[head] = "^";
+  return tape.join("") + "\n" + temp.join("");
 }
 
+/** Returns a (new_state, head_move) pair resulting from one step of the TM exitution on the given tape.
+ *   Also updates the tape appropriately in place. If the TM reads a symbol off the tape that does not
+ *   have an assioated transistion from the current state the next_state will be REJECT.
+ */
+export function tm_step(
+  tm: TM,
+  state: string,
+  tape: Array<string>,
+  head: number,
+): TM_Update {
+  const state_lookup = tm.delta.get(state)!; // delta.get(state) is garanteed to exist as it is checked in check_delta.
+  const transistion = state_lookup.get(tape[head]);
+  if (transistion === undefined) {
+    return { next_state: REJECT, head_move: 0 };
+  }
+  tape[head] = transistion!.write;
+  return {
+    next_state: transistion!.next,
+    head_move: transistion!.move === "R" ? 1 : -1,
+  };
+}
 
+/** Return A TM_Result object resulting from executing the given TM on the given input string.
+ */
 export function tm_execute(tm: TM, input: string): TM_Result {
   let tape = init_tape(input);
   //Copy input to tape
@@ -134,19 +172,13 @@ export function tm_execute(tm: TM, input: string): TM_Result {
     tape[TAPE_CHUNK_LEN + i] = input[i];
   }
   let head = TAPE_CHUNK_LEN;
-  let spaces = Array(tape.length).fill(" ");
-  
   let state = tm.start_state;
-  while (!tm.accept_states.has(state) && state != REJECT) {
-    //Print tape
-    let temp = [...spaces];
-    temp[head] = "^";
-    console.log(tape.join("") + "\n" + temp.join("")); 
+  while (!tm.accept_states.has(state) && state !== REJECT) {
     //Get new TM state
-    let update = tm_step(tm, state, tape, head);
+    const update = tm_step(tm, state, tape, head);
     state = update.next_state;
     head += update.head_move;
-    
+    //Extend tape if needed
     if (head < 0 || head >= tape.length) {
       tape = extend_tape(head, tape);
       if (head < 0) {
@@ -154,32 +186,8 @@ export function tm_execute(tm: TM, input: string): TM_Result {
       }
     }
   }
-  return {accept: state === ACCEPT ? ACCEPT : REJECT, on_tape: tape.filter((slot, i, t) => !should_trim(slot, i, t)).join("")};
+  return {
+    accept: tm.accept_states.has(state) ? ACCEPT : REJECT,
+    on_tape: tape.filter((slot, i, t) => !should_trim(slot, i, t)).join(""),
+  };
 }
-
-
-// let table = `q0 q1
-//              q0 a b R q0
-//              q0 b a R q0
-//              q0 _ _ R q1`;
-
-let table = `q0 q6
-             q0 x _ R q1
-             q0 _ _ R q6
-             q1 x x R q1
-             q1 y y R q1
-             q1 _ _ L q2
-             q2 x _ L q3
-             q3 x x L q3
-             q3 y y L q3
-             q3 _ _ R q0
-             q0 y _ R q4
-             q4 x x R q4
-             q4 y y R q4
-             q4 _ _ L q5
-             q5 y _ L q3`
-
-let tm = read_transtion_table(table);
-console.log(tm_execute(tm, "xyyx"));
-console.log(tm_execute(tm, "xyxy"));
-// console.log(tm_execute(tm, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
--- tests/unit/lib/turing/turing.test.ts
+++ tests/unit/lib/turing/turing.test.ts
@@ -6,11 +6,85 @@
 
 import { describe, expect, it } from "vitest";
 
-import {read_transtion_table} from "$lib/turing/turing";
+import { read_transtion_table, tm_execute } from "$lib/turing/turing";
 
 describe("Transision tables", () => {
   it("Reads the header", async () => {
     const tm = read_transtion_table(`q0 q1`);
-    expect(tm.start_state === "q0" && tm.accept_states.has("q1"));
+    expect(
+      tm.start_state === "q0" &&
+        tm.accept_states.has("q1") &&
+        tm.accept_states.size === 1 &&
+        tm.delta.size === 0,
+    ).toEqual(true);
+  });
+
+  it("Reads a simple tm", () => {
+    const tm = read_transtion_table(`q0 q1
+                                     q0 a b R q0
+                                     q0 b a R q0
+                                     q0 _ _ R q1`);
+    expect(
+      tm.delta.size === 1 &&
+        tm.delta.get("q0")?.get("a")?.write === "b" &&
+        tm.delta.get("q0")?.get("a")?.move === "R" &&
+        tm.delta.get("q0")?.get("a")?.next === "q0" &&
+        tm.delta.get("q1") === undefined,
+    ).toEqual(true);
+  });
+});
+
+describe("Execution", () => {
+  it("Reads and executes a simple tm", async () => {
+    const tm = read_transtion_table(
+      `q0 q1
+       q0 _ _ R q1`,
+    );
+    const result = tm_execute(tm, "");
+    expect(result.accept).toEqual("ACCEPT");
+    expect(result.on_tape).toEqual("");
+  });
+
+  it("Reads and executes a tm that 'nots' the input string", async () => {
+    const tm = read_transtion_table(`q0 q1
+                                     q0 a b R q0
+                                     q0 b a R q0
+                                     q0 _ _ R q1`);
+    const result = tm_execute(tm, "ababaab");
+    expect(result.accept).toEqual("ACCEPT");
+    expect(result.on_tape).toEqual("bababba");
+  });
+
+  it("Rejects inputs with symbols not in the tm's delta", async () => {
+    const tm = read_transtion_table(`q0 q1
+                                     q0 a b R q0
+                                     q0 b a R q0
+                                     q0 _ _ R q1`);
+    const result = tm_execute(tm, "abbxyy");
+    expect(result.accept).toEqual("REJECT");
+    expect(result.on_tape).toEqual("baaxyy");
+  });
+
+  it("Read and executes a tm that accepts binary palindromes", async () => {
+    const table = `q0 q6
+                   q0 x _ R q1
+                   q0 _ _ R q6
+                   q1 x x R q1
+                   q1 y y R q1
+                   q1 _ _ L q2
+                   q2 x _ L q3
+                   q3 x x L q3
+                   q3 y y L q3
+                   q3 _ _ R q0
+                   q0 y _ R q4
+                   q4 x x R q4
+                   q4 y y R q4
+                   q4 _ _ L q5
+                   q5 y _ L q3`;
+    const tm = read_transtion_table(table);
+    expect(tm_execute(tm, "").accept).toEqual("ACCEPT");
+    expect(tm_execute(tm, "xyyx").accept).toEqual("ACCEPT");
+    expect(tm_execute(tm, "xyxy").accept).toEqual("REJECT");
+    expect(tm_execute(tm, "x".repeat(100)).accept).toEqual("ACCEPT");
   });
 });
