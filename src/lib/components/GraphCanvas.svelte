9e1e97f571398fb0b4c5adfd96f21b7b580ad20b
goose_swansworth <fridgeineyes@gmail.com>
2023-01-07 13:34:28 +1300

    Edge labels can be selected, edited and moved with mouse

--- src/lib/components/GraphCanvas.svelte
+++ src/lib/components/GraphCanvas.svelte
@@ -15,7 +15,8 @@
     type Vec2d,
     type Edge,
     new_edge,
-    draw_edge
+    draw_edge,
+    edge_label_bounding_box
   } from "$lib/turing/graph";
 
 
@@ -26,15 +27,21 @@
   let mouse = { x: 0, y: 0 };
   let last_mouse = {x: 0, y: 0};
   let mouse_down_pos = { x: 0, y: 0 };
-  let moving = false;
+  let node_moving = false;
+  let edge_moving = false;
   let node_radius = 20;
 
   let mouse_down = false;
   let last_mouse_down: number;
   let moving_node_index: number;
+  let moving_edge_index: number;
 
-  let edit_index = -1;
+  let edit_node_index = -1;
+  let edit_edge_index = -1;
   let last_clicked_node = -1;
+
+  let key_down: string;
+  let key_code: string;
 
 
   let nodes: Array<Node> = [];
@@ -44,17 +51,32 @@
     context = canvas.getContext("2d")!;
     canvasStore.set(canvas);
     contextStore.set(context);
-    context.font = "10px serif";
+    context.font = "15px mono";
   });
 
   function in_circle(center: Vec2d, radius: number, mouse: Vec2d): boolean {
     return (center.x - mouse.x) ** 2 + (center.y - mouse.y) ** 2 <= radius ** 2;
   }
 
+  function in_rect(top_left: Vec2d, w: number, h: number, mouse: Vec2d): boolean {
+    return mouse.x >= top_left.x && mouse.x <= top_left.x + w && mouse.y >= top_left.y && mouse.y <= top_left.y + h;
+  }
+
   // Return the index of the node the mouse is in, otherwise return -1
   function selected_node(nodes: Array<Node>, mouse: Vec2d): number {
     for (const [i, node] of nodes.entries()) {
       if (in_circle(node.pos, node_radius, mouse)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // Return the index of the edge the mouse is in the label in, otherwise return -1
+  function selected_edge(edges: Array<Edge>, mouse: Vec2d): number {
+    for (const [i, edge] of edges.entries()) {
+      const edge_box = edge_label_bounding_box(context, edge);
+      if (in_rect(edge_box.top_left, edge_box.width, edge_box.height, mouse)) {
         return i;
       }
     }
@@ -78,8 +100,8 @@
       context.stroke();
       context.fillText(node.label, node.pos.x, node.pos.y)
     }
-    for (const edge of edges) {
-      draw_edge(context, edge, node_radius);
+    for (const [i, edge] of edges.entries()) {
+      draw_edge(context, edge, node_radius, i === edit_edge_index);
     }
 
   }
@@ -106,13 +128,17 @@
     const last_y = mouse.y;
     mouse.x = clientX - rect.left;
     mouse.y = clientY - rect.top;
-    if (moving) {
+    if (node_moving) {
       const dx = mouse.x - last_x;
       const dy = mouse.y - last_y;
       nodes[moving_node_index].pos.x += dx;
       nodes[moving_node_index].pos.y += dy;
-      redraw();
     }
+    if (edge_moving) {
+      const dy = mouse.y - last_y;
+      edges[moving_edge_index].h += dy;
+    }
+
     if (mouse_down) {
       redraw();
     }
@@ -120,28 +146,38 @@
 
   function handle_mouse_down(event: any) {
     mouse_down = true;
-    const selected_index = selected_node(nodes, mouse);
+    const selected_node_index = selected_node(nodes, mouse);
+    const selected_edge_index = selected_edge(edges, mouse);
+
     // Move node with mouse
-    if (selected_index >= 0) {
-      mouse_down_pos.x = mouse.x;
-      mouse_down_pos.y = mouse.y;
-      moving = true;
-      moving_node_index = selected_index;
+    if (selected_node_index >= 0) {
+      node_moving = true;
+      moving_node_index = selected_node_index;
       last_mouse.x = mouse.x;
       last_mouse.y = mouse.y;
     }
+
+    //Move edge with mouse
+    if (selected_edge_index >= 0) {
+      edge_moving = true;
+      moving_edge_index = selected_edge_index;
+    }
+
     handle_mouse_move(event);
     if (double_click()) {
-      if (selected_index >= 0) { // Edit node label
-        edit_index = selected_index;
+      if (selected_node_index >= 0) { // Edit node label
+        edit_node_index = selected_node_index;
+      } else if (selected_edge_index >= 0) {
+        edit_edge_index = selected_edge_index;
       } else { // Create a new node
         nodes.push(new_node({x: mouse.x, y: mouse.y}));
-        edit_index = -1;
+        edit_node_index = -1;
         last_clicked_node = -1;
       }
     }
-    else { // Unselect node
-      edit_index = -1;
+    else { // Unselect nodes and edges
+      edit_node_index = -1;
+      edit_edge_index = -1;
     }
     last_mouse_down = performance.now();
     redraw();
@@ -161,8 +197,23 @@
       last_clicked_node = -1;
     }
     mouse_down = false;
-    moving = false;
+    node_moving = false;
+    edge_moving = false;
     redraw();
+  }
+
+  function handle_key_down(event: any) {
+    key_down = event.key;
+    key_code = event.code;
+    if (edit_edge_index >= 0) {
+      if (key_down === "Backspace" && edges[edit_edge_index].label.length > 0) {
+        edges[edit_edge_index].label = edges[edit_edge_index].label.slice(0, edges[edit_edge_index].label.length - 1);
+        redraw();
+      } else if (key_down.length === 1) {
+        edges[edit_edge_index].label = edges[edit_edge_index].label.concat(key_down);
+        redraw();
+      }
+    }
   }
 </script>
 
@@ -170,6 +221,7 @@
   on:mousedown={handle_mouse_down}
   on:mousemove={handle_mouse_move}
   on:mouseup={handle_mouse_up}
+  on:keydown={handle_key_down}
 />
 
 <canvas
--- src/lib/turing/graph.ts
+++ src/lib/turing/graph.ts
@@ -6,10 +6,12 @@
 
 import { getContext, onMount, onDestroy } from "svelte";
 import { writable, derived } from "svelte/store";
+import type { E } from "vitest/dist/types-de0e0997";
 
 export const width = 732;
-export const height = 172;
+export const height = 2*172;
 export const ARROW_SIZE = 10;
+export const TEXT_BOX_MARGIN = 5;
 export const context = writable();
 export const canvas = writable();
 
@@ -19,51 +21,106 @@ export const minus = (u: Vec2d, v: Vec2d): Vec2d => ({x: u.x - v.x, y: u.y - v.y
 export const norm = (u: Vec2d): number => (Math.sqrt(u.x**2 + u.y**2));
 export const times = (a: number, u: Vec2d) => ({x: a * u.x, y: a * u.y});
 export const perp = (u: Vec2d) => ({x: -u.y, y: u.x});
+export const normed = (u: Vec2d) => (times(1/norm(u), u));
+
+export type LabelBoundingBox = { top_left: Vec2d, width: number, height: number};
 
 export type Node = { pos: Vec2d; label: string};
 
 export type Edge = { node1: Node, node2: Node, label: string, h: number};
 
 export const new_node = (center: Vec2d): Node => ({ pos: center, label: ""});
-export const new_edge = (node1: Node, node2: Node): Edge => ({node1, node2, label: "", h: 0});
+export const new_edge = (node1: Node, node2: Node): Edge => ({node1, node2, label: "q0, a, b, R", h: 0});
 
-function draw_straight_edge(context: CanvasRenderingContext2D, edge: Edge, node_radius: number) {
-  context.strokeStyle = "#FFFFFF";
-  //context.fillStyle = "#FFFFFF";
-
+function draw_curly_arrow(context: CanvasRenderingContext2D, tip: Vec2d, base: Vec2d) {
+  context.lineWidth = 1.5;
   const s = ARROW_SIZE;
+  const base_to_tip = times(1/norm(minus(base, tip)), minus(base, tip));
+  const b = plus(base, times(2*s/3, perp(base_to_tip)));
+  const c = plus(base, times(-2*s/3, perp(base_to_tip)));
+
+  context.beginPath();
+  context.moveTo(tip.x, tip.y);
+  context.quadraticCurveTo(base.x, base.y, b.x, b.y);
+  context.stroke();
+
+  context.beginPath();
+  context.moveTo(tip.x, tip.y);
+  context.quadraticCurveTo(base.x, base.y, c.x, c.y);
+  context.stroke();
+}
+
+export function edge_label_bounding_box(context: CanvasRenderingContext2D, edge: Edge): LabelBoundingBox {
+  const label_dims = context.measureText(edge.label);
+  const p1 = edge.node1.pos;
+  const p2 = edge.node2.pos;
+  const v = normed(minus(p2, p1));
+  const n = perp(v);
+  const mid = plus(p1, times(norm(minus(p2, p1))/2, v));
+  const label = plus(mid, times(edge.h, n));
+  return {
+    top_left: {x: label.x - label_dims.width/2 - TEXT_BOX_MARGIN, y: label.y - label_dims.actualBoundingBoxAscent - 2 * TEXT_BOX_MARGIN},
+    width: label_dims.width + 2*TEXT_BOX_MARGIN,
+    height: label_dims.actualBoundingBoxAscent + 2*TEXT_BOX_MARGIN
+  };
+}
+
+
+function draw_edge_label(context: CanvasRenderingContext2D, edge: Edge, editing: boolean) {
+  const p1 = edge.node1.pos;
+  const p2 = edge.node2.pos;
+  const v = normed(minus(p2, p1));
+
+  const n = perp(v);
+  const mid = plus(p1, times(norm(minus(p2, p1))/2, v));
+  const label = plus(mid, times(edge.h, n));
+
+  const TEXT_BOX_MARGIN = 5;
+  const label_dims = context.measureText(edge.label);
+  const width = label_dims.width;
+  const height = label_dims.actualBoundingBoxAscent;
+
+  if (editing) {
+    context.setLineDash([5, 5]);
+  }
+  context.fillStyle = "#1F2937";
+  context.fillRect(label.x - width/2 - TEXT_BOX_MARGIN, label.y - height - 2 * TEXT_BOX_MARGIN, width + 2 * TEXT_BOX_MARGIN, height + 2 * TEXT_BOX_MARGIN);
+  context.strokeRect(label.x - width/2 - TEXT_BOX_MARGIN, label.y - height - 2 * TEXT_BOX_MARGIN, width + 2 * TEXT_BOX_MARGIN, height + 2 * TEXT_BOX_MARGIN);
+  context.setLineDash([]);
+  context.fillStyle = "#FFFFFF";
+  context.fillText(edge.label, label.x - width/2, label.y - TEXT_BOX_MARGIN);
+
+}
+
+function draw_straight_edge(context: CanvasRenderingContext2D, edge: Edge, node_radius: number, editing: boolean) {
+  context.strokeStyle = "#FFFFFF";
+  context.fillStyle = "#FFFFFF";
+  const s = ARROW_SIZE;
+
   let p1 = edge.node1.pos;
   let p2 = edge.node2.pos;
   let p1_to_p2 = minus(p2, p1);
   const dist = norm(p1_to_p2);
   p1_to_p2 = times(1/dist, p1_to_p2);
+
   p1 = plus(p1, times(node_radius, p1_to_p2));
   p2 = plus(p1, times((dist - 2*node_radius), p1_to_p2));
   const p2_draw = plus(p1, times((dist - 2*node_radius) - 5, p1_to_p2));
   const m = minus(p2, times(Math.sqrt((3*s**2)/4), p1_to_p2));
-  const b = plus(m, times(2*s/3, perp(p1_to_p2)));
-  const c = plus(m, times(-2*s/3, perp(p1_to_p2)));
 
   context.beginPath();
   context.moveTo(p1.x, p1.y);
   context.lineTo(p2_draw.x, p2_draw.y);
   context.closePath();
   context.stroke();
+  draw_curly_arrow(context, p2, m);
+  draw_edge_label(context, edge, editing);
 
-  context.lineWidth = 1.5;
-  context.beginPath();
-  context.moveTo(p2.x, p2.y);
-  context.quadraticCurveTo(m.x, m.y, b.x, b.y);
-  context.stroke();
 
-  context.beginPath();
-  context.moveTo(p2.x, p2.y);
-  context.quadraticCurveTo(m.x, m.y, c.x, c.y);
-  context.stroke();
+
 }
 
-export function draw_edge(context: CanvasRenderingContext2D, edge: Edge, node_radius: number) {
-  if (edge.h === 0) {
-    draw_straight_edge(context, edge, node_radius);
-  }
+export function draw_edge(context: CanvasRenderingContext2D, edge: Edge, node_radius: number, editing: boolean) {
+  draw_straight_edge(context, edge, node_radius, editing);
+
 }
