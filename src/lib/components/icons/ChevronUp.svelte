37a49e024425466398a342debdefe1418ca2925e
Severen Redwood <me@severen.dev>
2023-02-06 00:24:09 +1300

    Add functionality to display reduction steps

---
+++ src/lib/components/icons/ChevronUp.svelte
@@ -0,0 +1,16 @@
+<!-- @license
+  SPDX-FileCopyrightText: 2020 Refactoring UI Inc.
+  SPDX-License-Identifier: MIT
+
+  Source: https://heroicons.com/
+-->
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+  class="h-6 w-6"
+>
+  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+</svg>
